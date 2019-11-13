98e7e39392e542e49a7edb6395f5e21bef221eae
taruc <taruc@B101>
2019-11-13 10:38:39 +0800

    [Delivers #169307208]

---
+++ src/sampleintegration/SampleIntegration.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleintegration;
+
+/**
+ *
+ * @author taruc
+ */
+public class SampleIntegration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Happy happy");
+        System.out.println("sad sad");
+    }
+
+}
